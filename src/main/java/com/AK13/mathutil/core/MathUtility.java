eace8d54e9804aeb259a79d9c36cfad9151b829b
Anhkiet1310 <nguyenvoanhkiet997@gmail.com>
2023-10-16 08:56:49 +0700

    update getf() to use recursion to see green3ever

--- src/main/java/com/AK13/mathutil/core/MathUtility.java
+++ src/main/java/com/AK13/mathutil/core/MathUtility.java
@@ -17,26 +17,57 @@ public class MathUtility {
 
     public static final double PI = 3.14;
 
+//    public static long getFactorial(int n) {
+//        //ham tinh n!=1,2,3,...n
+//        //ko tinh dc giai thua so am
+//        // quy uoc 0! la 1
+//        //vi giai thua tang rat nhanh kieu long chua ko noi(vuot 18 so 0)
+//        //do do' ham` getF() chi co n=0-20
+//        if (n < 0 || n > 20) 
+//            throw new IllegalArgumentException();
+//        
+//
+//        if (n == 0 || n == 1) 
+//            return 1;
+//        
+//
+//        long result = 1; //n>=2-<=20
+//        
+//        for (int i = 2; i <= n; i++) 
+//            result *= i;
+//        
+//
+//        return result;
+//    }
+//    5!=1*2*3*4 5
+//    5!= 4! *5=5*4!
+//    4!=3!*4=4*3!
+//    3!=2!*3=3*2!
+//    2!=2*1!=1!*2
+//    --> N!=N*(N-1)!
     public static long getFactorial(int n) {
-        //ham tinh n!=1,2,3,...n
-        //ko tinh dc giai thua so am
-        // quy uoc 0! la 1
-        //vi giai thua tang rat nhanh kieu long chua ko noi(vuot 18 so 0)
-        //do do' ham` getF() chi co n=0-20
-        if (n < 0 || n > 20) 
+
+        if (n < 0 || n > 20) {
             throw new IllegalArgumentException();
-        
+        }
 
-        if (n == 0 || n == 1) 
+        if (n == 0 || n == 1) {
             return 1;
-        
+        }
 
-        long result = 10; //n>=2-<=20
-        
-        for (int i = 2; i <= n; i++) 
-            result *= i;
-        
+        return n * getFactorial(n - 1);
 
-        return result;
+        /*
+        tối ưu code, thay ruột của hàm để chạy ngon hơn
+        nhanh hơn, code đẹp hơn
+        fix bug
+        dù sửa code vì lí do gì thì code phải được test lại
+        test lại để đảm bảo rằng nó vẫn xanh như lần nào
+        viết code đi kèm test scrip thì việc test lại hàm cực nhanh
+        ko cần dùng mắt dò từng dòng lệnh, chỉ cần xem EP==EV không
+        test lại những gì đã từng test -Regression test(kiểm thử hồi quy)
+        kiểm thử lần này cực nhanh vì github lo dùm
+        ta chỉ cần màu xanh hoặc nhận mail chửi khi đỏ
+         */
     }
 }
